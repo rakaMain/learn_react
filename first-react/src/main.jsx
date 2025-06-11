21ae644d9d91fb8e9584c7fed591e23e4dcadd4b
raka kayla <rakakayla7@gmail.com>
2025-06-11 13:55:11 +0700

    day 2 a

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -123,3 +123,123 @@
 // )
 
 //  
+
+
+//--------------------------- costum component chalange1 
+
+// import { createElement } from "react";
+// import { createRoot } from "react-dom/client"
+
+// function MyheadLine() {
+//   return <h3 className='font-bold'>Fun fuct about react </h3>
+// }
+
+// function MylistLine() {
+//   return (
+//     <ol className='mt-2 font-semibold '>
+//       <li>1. lorem ipsum dolor amet</li>
+//       <li>2. lorem ipsum dolor amet</li>
+//       <li>3. lorem ipsum dolor amet</li>
+//       <li>4. lorem ipsum dolor amet</li>
+//     </ol>
+//   )
+// }
+
+// function MainBodyList() {
+//   return (
+//   <div className='p-4'>
+//     <img src="src\assets\react.svg" className='mb-2'  alt="" srcset="" />
+//     <MyheadLine />
+//     <MylistLine />
+//   </div>
+//   )
+// }
+
+
+// const root = createRoot(document.getElementById("root"))
+// root.render(
+//   <MainBodyList />
+// )
+
+
+//--------------------------- costum component chalange2
+
+// import { createElement } from "react";
+// import { createRoot } from "react-dom/client"
+
+
+// function MyHeaderLine() {
+//   return (
+//     <header className='countainer flex justify-between h-20 w-full bg-[#213547] font-semibold items-center-safe px-7'>
+//       <div>
+//          <img src="src\assets\react.svg" alt="" srcset="" />
+//          <p>react</p>
+//       </div>
+//       <div>nav</div>
+//       <div>user</div>
+//     </header>
+//   )
+// }
+
+// function MyBodyLine() {
+//   return (
+//     <div className='container  p-2.5 h-[400px] w-full flex flex-col'>
+//       <h2 className='font-semibold text-2xl '> i like  reacet</h2>
+//       <div className='mt-7'>
+//         <ol className=''>
+//           <li> 1. suka saja </li>
+//           <li> 2. suka saja </li>
+//           <li> 3. suka saja </li>
+//           <li> 4. suka aaja </li>
+//         </ol>
+//       </div>
+//     </div>
+//   )
+// }
+
+// function MyFooterLine() {
+//   return (
+//     <footer className=' container w-full flex justify-center pt-[60px]'>
+//       <p> @ 2025 codingan by raka</p>
+//     </footer>
+//   )
+// }
+// function MyCodeLine(){
+//   return (
+//     <div>
+//          <MyHeaderLine /> 
+//          <MyBodyLine />
+//          <MyFooterLine />
+//     </div>
+//   )
+// }
+
+
+
+// const root = createRoot(document.getElementById("root"))
+// root.render(
+//   MyCodeLine()
+// )
+
+
+// ----------------fragment utntuk merapihkan div yyanng membungkus
+
+import { createElement } from "react";
+import { createRoot } from "react-dom/client"
+import { Fragment  } from 'react';
+
+function HeadMiLine(){
+  return (
+    <>
+        <header><p>thi header</p></header>
+        <main><h1>Halo</h1><p>Lorem, ipsum dolor sit amet co</p></main>
+        <footer><p>@ 2035 copyright </p></footer>
+    </>
+  )
+}
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  HeadMiLine()
+)
