9fac8dfa258c575cef7013971d212797b8992fd4
raka kayla <rakakayla7@gmail.com>
2025-06-13 17:41:25 +0700

    day 4 liteasi props

--- first-react/src/div.jsx
+++ first-react/src/div.jsx
@@ -1,19 +1,16 @@
 
-export default function MyBody(){
+export default function MyCardy(props){
   return(
     <>
-    <main className=' overflow-hidden mt-[12px] mx-auto rounded-2xl relative container px-7 items-start flex justify-center w-full bg-[#2f2f2f] h-[400px] flex-col'>
-       <h2 className=' text-3xl font-extrabold flex '> Reason why i'm Learn <p className='text-sky-700 ms-2'> REACT</p></h2>
-       <div className='mt-7'>
-         <ol className=' '>
-           <li> 1. suka saja </li>
-           <li> 2. suka saja ngebantu banget </li>
-           <li> 3. pengen di bilang keren </li>
-           <li> 4. karean gwa ganteng </li>
-         </ol>
-       </div>
-       <img src="src\assets\react.svg" className="absolute size-[300px]   opacity-5 end-[-70px] bottom-[-60px] " alt="" srcset="" />
-    </main>
+      <div className="relative w-[350px] mx-auto mt-3  h-40 flex gap-x-2" id="card">
+        <div className=" flex-1/4 me-2 bg-amber-400 rounded-xl"></div>
+        <div className="relative flex-1/2  flex flex-col h-full  py-4 text-black">
+            <p className=" font-semibold text-[10px]">JAPAN <span className=" text-gray-500  opacity-70 underline ms-2">see on google maps</span> </p>
+            <p className=" font-extrabold text-3xl mb-3">JUDUL</p>
+            <p className=" font-bold text-[10px]">12 Jan 2021 - 2024 Jan 2021</p>
+            <p className="text-[8px]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat sit officiis asperiores modi cupiditate dolor iste temporibus et nesciunt, est, id aliquam consequatur voluptate, maiores tempore magni facere minima! Id!</p>
+        </div>
+      </div>
     </>
   )
 }
--- first-react/src/header.jsx
+++ first-react/src/header.jsx
@@ -1,16 +1,8 @@
 export default function MyHead(){
   return(
-    <header className='rounded-b-2xl mx-auto relative container px-7 items-center-safe  flex justify-between w-full bg-[#2f2f2f] h-20'>
-        <img src="src\assets\react.svg" className='size-[35x] ' alt="" />
-        <ul className='font font-semibold text-[12px] flex justify-around  w-64'>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-        </ul>
-        <div className='flex flex-col justify-center'>
-        <div className='bg-sky-700 h-7 w-7 rounded-full'></div>
-        <p className='font-bold '>user</p>
-        </div>
+    <header className='relative w-full h-14 bg-red-400 flex items-center justify-center gap-3 '>
+      <div className="w-5 h-5 relative rounded-full bg-amber-50 "></div>
+      <p className="relative text-sm">My Travel joural.</p>
     </header>    
     )
 }
