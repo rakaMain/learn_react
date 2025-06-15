94767e44d30325002568f7a2e94dc44250b4356a
raka kayla <rakakayla7@gmail.com>
2025-06-15 08:31:01 +0700

    day 06 2

---
+++ first-react/src/data_anime.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        img : "src\assets\gambar\Black Clover Shounen Shoump.jfif",
+        nama : "Black Clover",
+        kanji : "ブラッククローバー",
+        date : "3 Oktober 2017",
+        mangaka : "Yuki Tabata",
+        rating : "8.1 (MyAnimeList)",
+        desk : " Di dunia sihir, Asta adalah anak tanpa kekuatan sihir, tapi bermimpi menjadi Kaisar Sihir. Bersama saingannya, Yuno, ia bertarung melawan kekuatan gelap."
+    },
+    {
+        img : "src/aassets/ggambar/ssakamoto days ch_ 113 color page.jfif",
+        nama : "Sakamoto Days",
+        kanji : "サカモトデイズ",
+        date : "3 juni 2025",
+        mangaka : "Yuto Suzuk",
+        rating : "8.3 (MyAnimeList)",
+        desk : "Sakamoto, mantan pembunuh bayaran legendaris, kini hidup damai sebagai pemilik toko. Namun, masa lalunya terus membayanginya dan memaksanya kembali ."
+    }
+]
