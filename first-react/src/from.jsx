02a588b45bd0b631e489b5dd9416f74879fc4e90
raka <rakakayla7@gmail.com>
2025-06-18 08:39:13 +0700

    adding event in react day 08

--- first-react/src/from.jsx
+++ first-react/src/from.jsx
@@ -1,17 +1,44 @@
 import './index.css'
 
 export default function MyForm(){
+
+  const ingredient = ["Chicken", "Oregano", "Tomatoes"]
+
+  const ListInggredient = ingredient.map( n => (
+    <li className=" text-[13px]  self-start" key={n}>-- {n}</li>
+  ))
+  
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient_form")
+    ingredient.push(newIngredient)
+    console.log(ingredient)
+
+
+  }
+
   return(
-        <div id="form" className="w-full h-16 flex justify-center bg-whte items-center gap-x-2">
-            <form action="" id="add-ingredient" className="flex justify-center items-center">
+        <div id="form" className="w-full h-auto flex-col flex justify-center bg-whte items-center gap-x-2">
+            <form action="" id="add-ingredient" 
+            className="flex justify-center items-center h-17"
+            onSubmit={handleSubmit}
+            >
                 <input 
                 type="text" 
                 aria-label="Add ingredient"
                 placeholder="e. g. oregano"
-                className=" border-1 border-gray-200 rounded-md h-6 text-sm p-3 "
+                className=" border-1 border-gray-200 rounded-md h-6 text-sm p-3 w-65 "
+                name='ingredient_form'
+                
                 />
-                <button className=" h-6 w-16 bg-black rounded-md overflow-hidden ms-2  text-white flex justify-center items-center"><p className="text-[12px]">+ add</p></button>
+                <button 
+                className=" h-6 w-30 bg-black rounded-md overflow-hidden ms-2 text-[10px]  text-white flex justify-center items-center">+ add ingredient</button>
             </form>
+            <ul className='  relative flex  flex-col w-95 lining-nums '>
+                {ListInggredient}
+            </ul>
         </div>
     )   
 }
--- first-react/src/header.jsx
+++ first-react/src/header.jsx
@@ -1,7 +1,7 @@
 export default function MyHead(){
   return(
     <header className='relative w-full h-16 bg-white shadow-sm flex items-center justify-center gap-3 '>
-      <i class="ri-book-open-line scale-125"></i>
+      <i className="ri-book-open-line scale-125"></i>
       <p className="relative text-md">Chef Claude </p>
     </header>    
     )   
