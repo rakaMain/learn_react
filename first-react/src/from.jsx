e66cf0e3bd5de8e46e7747400ac3c62dd96e6036
raka <rakakayla7@gmail.com>
2025-06-20 19:57:00 +0700

    day 10 ternati and state

--- first-react/src/from.jsx
+++ first-react/src/from.jsx
@@ -3,22 +3,26 @@ import React from 'react'
 
 export default function MyForm(){
 
-  const [ingredient, setiIngredient] = React.useState(["Chicken", "Oregano", "Tomatoes"])
+  //  let [ingredient, setiIngredient] = React.useState(["Chicken", "Oregano", "Tomatoes"])
+ let [ingredient, setiIngredient] = React.useState(true)
 
-  const ListInggredient = ingredient.map( n => (
-    <li className=" text-[13px] text-black self-start" key={n}><i className='ri-price-tag-3-fill me-1'></i>{n}</li>
-  ))
-  
+  const value = ingredient ? "yes" : "no"
 
   function handleSubmit(event) {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
     const newIngredient = formData.get("ingredient_form")
-    newIng = ingredient.push(newIngredient)
-    setiIngredient(newIng)
-    console.log(newIng)
+    // ingredient.push(newIngredient)
+    // setiIngredient([...ingredient, newIngredient])
+    // setiIngredient(old => [...old, newIngredient])
+    setiIngredient( old => old ? false : true)
+  
+    
   }
 
+  // const ListInggredient = ingredient.map( n => (
+  //   <li className=" text-[13px] text-black self-start" key={n}><i className='ri-price-tag-3-fill me-1'></i>{n}</li>
+  // ))
   return(
         <div id="form" className="w-full h-auto flex-col flex justify-center bg-whte items-center gap-x-2">
             <form action="" id="add-ingredient" 
@@ -37,7 +41,9 @@ export default function MyForm(){
                 className=" h-6 w-30 bg-black rounded-md overflow-hidden ms-2 text-[10px]  text-white flex justify-center items-center">+ add ingredient</button>
             </form>
             <ul className='  relative flex  flex-col w-95 lining-nums '>
-                {ListInggredient}
+                {/* {ListInggredient} */}
+                {/* {ingredient} */}
+                {value}
             </ul>
         </div>
     )   
