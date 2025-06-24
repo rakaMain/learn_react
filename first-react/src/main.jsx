e560d243a575c3d96e485c66d8f9d50b7f53dd65
raka <rakakayla7@gmail.com>
2025-06-24 09:22:54 +0700

    day 13 membuat radio

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -9,7 +9,6 @@ import MyForm from "./from";
 import './index.css'
 import "remixicon/fonts/remixicon.css";
 
-
  
 function Mycode(){
 
@@ -27,31 +26,56 @@ function Mycode(){
 
     const email = formData.get('email')
     const pass = formData.get('password')
+    const Employment = formData.get('employment')
     
-    console.log(email, pass)
+    console.log(`${email} ,${pass}, ${Employment}`)
   }
   return(
     <div className="w-full container h-auto pt-6">
     <div className=" relative overflow-hidden p-5 flex-col justify-center mx-auto gap-1.5 w-80 h-auto rounded-2xl shadow-xl bg-gray-100">
       <div className="h-76 w-76 absolute rounded-full bg-gray-200 end-[-100px] bottom-[-100px]"></div>
       <p className=" ms-3 mb-5">Signup Form</p>
+
       <section>
         <form action={handlesubmit} className="mb-3 scale-90" >
           <label htmlFor="email">Email : </label>
           <input type="email" name="email" placeholder="example@gmail.com" className=" shadow rounded-md line-clamp-2  border-2 p-1 w-64"/>
           <br />
+
           <label htmlFor="password">Password : </label>
           <input type="password" name="password" placeholder="Abcd12345!" className=" shadow rounded-md  line-clamp-2 border-2 p-1 w-64"/>
+          <br />
 
-          <textarea className=" w-64 h-auto block bg-gray-100 border-2 rounded-md mt-4" name="" id=""></textarea>
+          <label htmlFor="Description">Description : </label>
+          <textarea className=" w-64 h-auto block bg-gray-100 border-2 rounded-md " name="Description" id="Descrption"></textarea>
+          
+          <fieldset className="mt-4 w-64 p-2 rounded-md border-black border">
+            <legend>Employment Status:</legend>
+            <label className=" me-3 mt-4" htmlFor="">
+              <input className="me-2" type="radio" name="employment" value='Chapcta' />
+              Chapcta
+            </label>
+            <label className=" mt-4" htmlFor="">
+              <input className="me-2" type="radio" name="employment" value='Federasi'/>
+              Federasi
+            </label>
+          </fieldset>
+
           <button className=" border-2 rounded-xl my-5 text-sm p-2 hover:bg-black hover:text-white "> submit</button>
+
         </form>
       </section>
     </div>
     </div>
   )
 }
+
 const root = createRoot(document.getElementById("root"))
 root.render(
   Mycode()
 ) 
+
+
+
+
+
